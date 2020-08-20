95f5755c4d1a7554452a5b6185672a5bb58fe9c5
fyodorschnotz <fyodorschnotz@gmail.com>
2020-08-20 21:39:37 +0700

    Menambah list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti laudantium tempore veniam perferendis nam! Cumque necessitatibus ratione impedit ducimus sit perferendis nihil perspiciatis, accusantium, officia commodi, eius similique esse optio?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
